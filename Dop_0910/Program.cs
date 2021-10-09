fe160d337c6344387ae90a0ce1b82165864b9846
shipilo <shadowfire-1@mail.ru>
2021-10-09 13:41:11 +0300

    Сделала интересное задание(предварительно)

--- Dop_0910/Program.cs
+++ Dop_0910/Program.cs
@@ -36,9 +36,40 @@ namespace Dop_0910
                 "Универ"
             };
             char separator = ' ';
+            char[] lettersRussian = { 'a', 'е', 'о', 'р', 'с', 'у', 'х', 'А', 'Е', 'О', 'Р', 'С', 'Х', 'В', 'К', 'М', 'Т' };
+            char[] lettersEnglish = { 'a', 'e', 'o', 'p', 'c', 'y', 'x', 'A', 'E', 'O', 'P', 'C', 'X', 'B', 'K', 'M', 'T' };
 
-            string stringOut = coloumn_0[rnd.Next(0, coloumn_0.Count)] + separator + coloumn_1[rnd.Next(0, coloumn_1.Count)];
+            string stringOut = coloumn_0[rnd.Next(coloumn_0.Count)] + separator + coloumn_1[rnd.Next(0, coloumn_1.Count)];
+            Dictionary<int, int> letterIndexes = new Dictionary<int, int>();
 
+            for (int i = 0; i < stringOut.Length; i++)
+            {
+                int index = Array.IndexOf(lettersRussian, stringOut[i]);
+                if (index != -1)
+                {
+                    letterIndexes.Add(i, index);
+                }
+            }
+
+            int indexRnd = -1, indexOfCharRnd = -1;
+            while (indexRnd == -1)
+            {
+                foreach (int index in letterIndexes.Keys)
+                {
+                    if (rnd.Next(0, 2) == 0)
+                    {
+                        indexRnd = index;
+                        indexOfCharRnd = letterIndexes[index];
+                        break;
+                    }
+                }
+            }
+            stringOut = String.Concat(stringOut.Substring(0, indexRnd), lettersEnglish[indexOfCharRnd], stringOut.Substring(indexRnd + 1), separator, coloumn_2[rnd.Next(coloumn_2.Count)]);
+            indexRnd = rnd.Next(stringOut.Length);
+            stringOut = String.Concat(stringOut.Substring(0, indexRnd), separator, stringOut.Substring(indexRnd));
+            Console.WriteLine(stringOut);
+
+            Console.ReadLine();
         }
     }
 }
