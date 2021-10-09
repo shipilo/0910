f91d75b1434948b266c0267a93bbd4e6ce7db752
shipilo <shadowfire-1@mail.ru>
2021-10-09 12:30:12 +0300

    Добавьте файлы проекта.

---
+++ Dop_0910/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dop_0910
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            List<string> coloumn_0 = new List<string>() {
+                "Это текст",
+                "Это материал",
+                "Это статья",
+                "Это публикация",
+                "Это данные",
+                "Это информация",
+                "Материал",
+                "Текст",
+                "Статья",
+                "Публикация",
+                "Данные",
+                "Информация"
+            };
+            List<string> coloumn_1 = new List<string>() {
+                "сайта",
+                "книги",
+                "библиотеки",
+                "каталога",
+                "системы"
+            };
+            List<string> coloumn_2 = new List<string>() {
+                "Univer",
+                "Univer.ququ",
+                "Универ"
+            };
+            char separator = ' ';
+
+            string stringOut = coloumn_0[rnd.Next(0, coloumn_0.Count)] + separator + coloumn_1[rnd.Next(0, coloumn_1.Count)];
+
+        }
+    }
+}
---
+++ Les_0910/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Les_0910
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
